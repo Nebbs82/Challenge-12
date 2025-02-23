1482dbf1e13f48bb40090434f487711d023b72be
Nebbs82 <nathan.r.ebbesen@hotmail.com>
2025-02-23 15:38:26 -0600

    final commit with downloadable resume

--- 02-Challenge/updated-portfolio/src/App.jsx
+++ 02-Challenge/updated-portfolio/src/App.jsx
@@ -3,7 +3,6 @@ import Header from './Header.jsx';
 import About from './About.jsx';
 import Portfolio from './Portfolio.jsx';
 import Contact from './Contact.jsx';
-import Resume from './Resume.jsx';
 import Footer from './Footer.jsx';
 
 
@@ -16,7 +15,6 @@ function App() {
         <About />
         <Portfolio />
         <Contact />
-        <Resume />
       <div>
         <Footer />
       </div>
